026f29fcfbb4eee736cec6f85a06b55886f7a847
Neous1 <Neous1@users.noreply.github.com>
2017-06-15 19:13:17 -0400

    object prototype

--- app.js
+++ app.js
@@ -3,5 +3,16 @@ function Person(firstname, lastname){
     this.lastname = lastname;
 }
 
-var Peter = new Person("john","Doe");
-console.log(Peter.firstname)
+Person.prototype.greet = function(){
+    console.log("Hello , " + this.firstname + " " + this.lastname);
+};
+
+var john = new Person("john","Doe");
+john.greet();
+
+var jane = new Person('Jane', 'Doe')
+jane.greet()
+
+console.log(john.__proto__);
+console.log(jane.__proto__);
+console.log(john.__proto__ ===jane.__proto__);
--- greet.js
+++
@@ -1,6 +0,0 @@
-var greet = function (){
-    console.log("hi from exported greet.js")
-};
-
-// make greet() available 
-module.exports = greet;
